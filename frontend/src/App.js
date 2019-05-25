9d33facc4b0e2f348533ecbc05bfc3fef83c574d
revoruxy <revoruxy@gmail.com>
2019-05-25 23:57:41 +0300

    Added the landing page as react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Signin from "./components/signin";
 import Signup from "./components/signup";
+import Ridehistory from "./components/ridehistory";
+import Rideoffer from "./components/rideoffer";
 
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Signup />
-    </div>
+    <Route>
+      <div>
+        <Rideoffer />
+      </div>
+    </Route>
   );
 }
 
---
+++ frontend/src/components/rideoffer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Profile from "../image/logo.png";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Rideoffer = () => {
+  return (
+    <div>
+      <div class="map">
+        <div class="map1">
+          <input type="text" placeholder="where to" />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Rideoffer;
